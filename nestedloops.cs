d849bd7e2780e98f9a008bf8e244eddbba9973c9
Hossam <hossamyasser1423@gmail.com>
2026-03-19 13:41:15 +0300

    All files fixed up

--- nestedloops.cs
+++ nestedloops.cs
@@ -1,25 +1,25 @@
-using System; 
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("How many rows would you like?: ");
+        Console.Write("How many rows would you like? ");
         int rows = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("How many columns would you like?: ");
+        Console.Write("How many columns would you like? ");
         int columns = Convert.ToInt32(Console.ReadLine());
 
         Console.Write("What symbol would you like to use?: ");
         string symbol = Console.ReadLine();
 
-        for (int i= 0; i < rows; i++)
+        for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < columns; j++)
             {
                 Console.Write(symbol);
             }
-        Console.WriteLine();
+            Console.WriteLine();
         }
 
 
